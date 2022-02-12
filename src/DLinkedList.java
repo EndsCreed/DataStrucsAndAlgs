18cff5d2827eeb83ad8544df639d916b61944833
EndsCreed <jonjondm2001@gmail.com>
2022-02-12 04:27:33 -0330

    Mostly done. Just some weird stuff with the removal method

--- src/DLinkedList.java
+++ src/DLinkedList.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class DLinkedList {
 
 	private DNode head;
@@ -5,37 +7,143 @@ public class DLinkedList {
 	int size;
 
 	public DLinkedList() {
-		/* Complete this method */
+		head = tail = null;
+		size = 0;
 	}
 
 	public boolean isEmpty() {
-		if(size == 0)
-			return true;
-		else
-			return false;
+		return size == 0;
 	}
 
-	public void insert(int n) {
-		/* Complete this method */
+	public void insert(int data) {
+		DNode n = new DNode();
+		n.setData(data);
+		if (isEmpty()) {
+			head = n;
+		} else {
+			tail.setNextLink(n);
+			n.setPrevLink(tail);
+		}
+		tail = n;
+		size++;
 	}
 	
 	public boolean remove() {
-		/* Complete this method */
+		if (isEmpty()) {
+			System.out.println("Cannot remove. List is already empty");
+			return false;
+		}
+		if (size == 1) {
+			head = tail = null;
+			size = 0;
+		} else {
+			tail = tail.getPrevLink();
+			tail.setNextLink(null);
+			size--;
+		}
+		return true;
 	}
 	
 	public boolean remove(int n) {
-		/* Complete this method */
+		if (isEmpty()) { // If empty, don't try and remove.
+			System.out.println("Cannot remove. List is already empty");
+			return false;
+		}
+		if (size == 1) {
+			if (head.getData() != n) { // If size 1 but n doesn't match the data, don't remove.
+				System.out.println("Specified integer not found. Failed to remove.");
+				return false;
+			}
+			// Otherwise, remove.
+			head = tail = null;
+			size = 0;
+		} else {
+			DNode temp = head;
+			if (temp != null && temp.getData() == n) { // In case the first entry is n.
+				head = temp.getNextLink();
+				size--;
+				return true;
+			}
+			while (temp.getNextLink() != null) {
+				if (temp.getData() == n) {
+					temp.getPrevLink().setNextLink(temp.getNextLink()); // Bridge the previous link forwards over the temp.
+					temp.getNextLink().setPrevLink(temp.getPrevLink()); // Bridge the next link backwards over the temp.
+					size--;
+					return true;
+				}
+			}
+			if (tail.getData() == n) { // The while loop won't ever check the tail. This checks the tail at the end.
+				tail = tail.getPrevLink();
+				tail.getNextLink().setPrevLink(null);
+				tail.setNextLink(null);
+				size--;
+				return true;
+			}
+		}
+		System.out.println("Specified integer not found. Failed to remove.");
+		return false; // If the list isn't empty, it isn't 1 with a matching value, the value isn't found from head to tail - 1, and thw tail isn't the value. It must not exist.
 	}
 
 	String toStringForward() {
-		/* Complete this method */
+		String result;
+		int[] list;
+		list = new int[size];
+		if (!isEmpty()) {
+			DNode temp = head;
+			for (int i = 0; temp != null; i++) {
+				list[i] = temp.getData();
+				temp = temp.getNextLink();
+			}
+			result = Arrays.toString(list);
+		} else {
+			result = "List is empty.";
+		}
+		return result;
 	}
 
 	String toStringReverse() {
-		/* Complete this method */
+		String result;
+		int[] list;
+		list = new int[size];
+		if (!isEmpty()) {
+			DNode temp = tail;
+			for (int i = 0; temp != null; i++) {
+				list[i] = temp.getData();
+				temp = temp.getPrevLink();
+			}
+			result = Arrays.toString(list);
+		} else {
+			result = "List is empty.";
+		}
+		return result;
 	}
 
 	public static void main(String[] args) {
-		/* Write your test code here */
+		DLinkedList dll = new DLinkedList();
+		dll.insert(3);
+		dll.insert(65);
+		dll.insert(21);
+		System.out.println(dll.toStringForward());
+		System.out.println(dll.toStringReverse());
+		dll.remove(64);
+		System.out.println(dll.toStringForward());
+		dll.remove(65);
+		System.out.println(dll.toStringForward());
+		dll.insert(117);
+		System.out.println(dll.toStringForward());
+		dll.remove();
+		System.out.println(dll.toStringForward());
+		dll.remove();
+		System.out.println(dll.toStringForward());
+		dll.remove();
+		System.out.println(dll.toStringForward());
+		dll.remove();
+		System.out.println(dll.toStringForward());
+		dll.insert(21);
+		System.out.println(dll.toStringForward());
+		dll.remove(32);
+		dll.remove(21);
+		dll.remove(21);
+		System.out.println(dll.toStringForward());
 	}
 }
--- src/DNode.java
+++ src/DNode.java
@@ -1,37 +1,18 @@
 public class DNode {
-    
-	/* Insert Member Variables Here */
-
+    private int data;
+	private DNode pLink;
+	private DNode nLink;
 
 	public DNode() {
-		/* Complete this method */
+		data = 0;
+		pLink = null;
+		nLink = null;
 	}
 
-    public void setData(int data) {
-		/* Complete this method */
-    }
-	
-	public int getData() {
-		/* Complete this method */
-    }
-    
-	public void setNextLink(DNode link) {
-		/* Complete this method */
-    }
-    
-	public DNode getNextLink() {
-		/* Complete this method */
-    }
-	
-	public void setPrevLink(DNode link) {
-		/* Complete this method */
-    }
-    
-	public DNode getPrevLink() {
-		/* Complete this method */
-    }
-
-	public static void main(String[] args) {
-		/* Write any test code here */
-	}
+    public void setData(int data) { this.data = data; }
+	public int getData() { return data; }
+	public void setNextLink(DNode link) { this.nLink = link; }
+	public DNode getNextLink() { return nLink; }
+	public void setPrevLink(DNode link) { this.pLink = link; }
+	public DNode getPrevLink() { return pLink; }
 }
