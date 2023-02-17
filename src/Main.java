c277819680b4198dfc40dcf7ce485e0fb9c6a6c1
EndsCreed <jonjondm2001@gmail.com>
2023-02-17 11:04:16 -0330

    Lab 3 Completed

--- src/Main.java
+++ src/Main.java
@@ -1,65 +1,98 @@
-import java.util.*;
+import java.util.Arrays;
 
-public class Main {
-    public static void main(String[] args) {
-        CircularQueue queue = new CircularQueue();
-        queue.enqueue(1);
-        queue.enqueue(2);
-        queue.enqueue(3);
-        queue.enqueue(4);
-        queue.enqueue(5);
-        queue.enqueue(6);
-        System.out.println("Dequeued: " + queue.dequeue());
-        System.out.println("Dequeued: " + queue.dequeue());
-        queue.enqueue(7);
-        queue.enqueue(8);
-        queue.enqueue(9);
-        queue.enqueue(10);
-        queue.enqueue(11);
-        queue.enqueue(12);
-        queue.enqueue(13);
-        queue.enqueue(14);
-        System.out.println("Dequeued: " + queue.dequeue());
-        System.out.println("Dequeued: " + queue.dequeue());
-        System.out.println("Dequeued: " + queue.dequeue());
-        System.out.println("Dequeued: " + queue.dequeue());
-        queue.enqueue(15);
-        System.out.println(queue.print());
+class Stack {
+    // Lab requires max of 5
+    static int MAX = 5;
+    int top;
+
+    // Changed to a 2D array that contains ann array of int arrays.
+    // Both single entries and pairs can now be ordered together.
+    int[][] array;
+    public Stack() {
+        // Define max size.
+        array = new int[MAX][];
+        top = -1;
+    }
+    public Stack(int capacity) {
+        // Allows for a specific capacity.
+        // Since the lab requires max size of 5
+        // the MAX will be set to the capacity
+        // only if the entered capacity is <= 5
+        MAX = Math.min(capacity, MAX);
+        array = new int[MAX][];
+        top = -1;
+    }
+    public boolean isEmpty() {
+        return (top < 0);
+    }
+    // For single int entries.
+    // This method creates an int array with one
+    // element and then increments top and pushes
+    // that into onto the stack
+    // Both push methods were changed to void because
+    // the bool function was not being utilized.
+    public void push(int x) {
+        if (top >= (MAX - 1)) {
+            System.out.println("Stack overflow");
+        } else {
+            array[++top] = new int[] {x};
+            System.out.println(x + " pushed into stack");
+            displayStack();
+        }
+    }
+    // Does the same as the above push method
+    // but instead takes two ints as parameters
+    // and makes an int[] with 2 elements to push
+    // onto the stack.
+    public void push(int x, int y) {
+        if (top >= (MAX - 1)) {
+            System.out.println("Stack overflow");
+        } else {
+            array[++top] = new int[] {x, y};
+            System.out.println("Pair (" + x + ", " + y + ") pushed to stack");
+            displayStack();
+        }
+    }
+    // Sets a temp variable to the value at the current
+    // top then decrements top. Then returns the value
+    // stored in the temp variable.
+    // Will return an int[] of size 0 if the stack is
+    // empty.
+    public int[] pop() {
+        if (isEmpty()) {
+            System.out.println("Stack underflow");
+            return new int[0];
+        } else {
+            int[] x = array[top--];
+            System.out.println(Arrays.toString(x) + " popped from stack");
+            displayStack();
+            return x;
+        }
+    }
+    // Iterates through the array and returns the string
+    // value of the array stored at each index.
+    public void displayStack() {
+        System.out.print("Stack: ");
+        if (isEmpty())
+            System.out.print("[]");
+        for (int i = top; i >= 0; i--) {
+            System.out.print(Arrays.toString(array[i]) + " ");
+        }
+        System.out.println();
     }
 }
-
-class CircularQueue {
-    int front, rear, size;
-    List<Integer> array;
-
-    /*
-    Removed the capacity arguement since this queue dynamically expands.
-    I can just use array.getSize() to find the size
-     */
-    public CircularQueue() {
-        front = rear = 0; // Front will always be 0, rear will be the last index of the array.
-        array = new ArrayList<>(); // Use an ArrayList Object instead so I can dynamically expand it.
-    }
-    public void enqueue(int data) {
-        if (array.size() >= 10) { // If the array lists size is 10 or more, refuse to add more objects.
-            System.out.println("Queue is full");
-        } else { // Otherwise, append the data to the end of the array and increment rear.
-            array.add(data);
-            rear++;
-        }
-    }
-    public int dequeue() {
-        if (array.isEmpty()) { // If the array is empty, refuse to attempt removal of object and return -1.
-            System.out.println("Queue is empty");
-            return -1;
-        } else {
-            int temp = array.get(front); // Temp variable to return after removal of the front object
-            array.remove(front); // Remove the first object
-            rear--; // Decrement rear since the last index value will drop by 1.
-            return temp; // Return what the first objects value was.
-        }
-    }
-    public String print() {
-        return Arrays.toString(array.toArray());
+class Main {
+    public static void main(String[] args) {
+        Stack stack = new Stack();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3,5);
+        stack.push(4,437);
+        stack.push(5);
+        System.out.println("Popped item: " + Arrays.toString(stack.pop()) + "\n");
+        System.out.println("Popped item: " + Arrays.toString(stack.pop()) + "\n");
+        System.out.println("Popped item: " + Arrays.toString(stack.pop()) + "\n");
+        System.out.println("Popped item: " + Arrays.toString(stack.pop()) + "\n");
+        System.out.println("Popped item: " + Arrays.toString(stack.pop()) + "\n");
     }
 }
